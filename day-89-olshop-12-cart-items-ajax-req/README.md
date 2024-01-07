48c752fc284b3c37b8477573a991faaf7619282b
yuliussetyawan <yuliusetyawan@gmail.com>
2024-01-07 23:55:54 +0700

    merge day-89

---
+++ day-89-olshop-12-cart-items-ajax-req/README.md
@@ -0,0 +1,26 @@
+# 100 Days Of Code - 2023 Web Development Bootcamp
+
+## Day 89: Building a Dynamic Shopping Cart
+
+Welcome to Day 89 of my coding journey! Today was all about laying the foundation for a seamless shopping experience by empowering customers to easily add items to their carts. Here's a breakdown of the highlights:
+
+**Day 89 Highlights:**
+
+* **Structuring the Shopping Cart:** Created a dedicated cart model to effectively manage and store information about items selected by customers, ensuring data organization and smooth checkout processes.
+
+* **Implementing Cart Logic:** Began developing the core logic behind the shopping cart, handling product additions, quantity updates, and potential removals, ensuring a user-friendly and intuitive experience.
+
+* **Dynamic Additions with Ajax:** Enabled users to add items to their carts without full page reloads, leveraging Ajax requests to create a more responsive and seamless interaction, fostering a sense of instant gratification.
+
+![Day 89 Preview](preview.png)
+
+**Stay tuned as I continue to refine the shopping cart experience, implementing quantity adjustments, visual feedback, and cart persistence for a truly user-centric shopping journey! ️**
+
+## Connect With Me
+
+- **Twitter**: @ylssty17: [https://twitter.com/ylssty17](https://twitter.com/ylssty17)
+- **LinkedIn**: Yulius Setyawan: [https://linkedin.com/in/yulius17](https://linkedin.com/in/yulius17)
+
+Let's keep learning and building engaging and efficient online shopping experiences together!
+
+\
---
+++ day-89-olshop-12-cart-items-ajax-req/controllers/admin.controller.js
@@ -0,0 +1,80 @@
+const Product = require("../models/product.model");
+
+async function getProducts(req, res, next) {
+  try {
+    const products = await Product.findAll();
+    return res.render("admin/products/all-products", { products: products });
+  } catch (error) {
+    next(error);
+    return;
+  }
+}
+
+function getNewProduct(req, res) {
+  res.render("admin/products/new-product");
+}
+
+async function createNewProduct(req, res) {
+  const product = new Product({
+    ...req.body,
+    // req.file because multer
+    image: req.file.filename,
+  });
+
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.redirect("/admin/products");
+}
+
+async function getUpdateProduct(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render("admin/products/update-product", { product: product });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    _id: req.params.id,
+  });
+  if (req.file) {
+    // replace the old image with the new one
+    product.replaceImage(req.file.filename);
+  }
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.redirect("/admin/products");
+}
+
+async function deleteProduct(req, res, next) {
+  let product;
+  try {
+    product = await Product.findById(req.params.id);
+    await product.remove();
+  } catch (error) {
+    return next(error);
+  }
+  // because we use AJAX request, there is no need to redirect
+  // res.redirect('/admin/products')
+  res.json({message: 'Deleted Product!'})
+}
+
+module.exports = {
+  getProducts: getProducts,
+  getNewProduct: getNewProduct,
+  createNewProduct: createNewProduct,
+  getUpdateProduct: getUpdateProduct,
+  updateProduct: updateProduct,
+  deleteProduct: deleteProduct,
+};
